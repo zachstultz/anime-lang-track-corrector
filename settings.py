f6facf2a81607618e9828e3563ca6ac4dc9bfce1
Zach Stultz <hcazs4@live.com>
2023-06-10 08:56:39 -0500

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,5 @@
+# The linux location for SE
+path_to_subtitle_edit_linux = "/PATH/TO/SUBTITLE/EDIT/FOLDER"
+
+# The name of the fasttext model in the root of the script folder
+fasttext_model_name = "lid.176.bin"
