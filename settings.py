af324db7931dcd83bc106ea4c4b0f68f2159383f
Zach Stultz <hcazs4@live.com>
2023-06-13 09:59:35 -0500

    Moved se linux back to main script, and model default

--- settings.py
+++ settings.py
@@ -1,8 +1,5 @@
-# The linux location for SE
-path_to_subtitle_edit_linux = "/PATH/TO/SUBTITLE/EDIT/FOLDER"
-
 # The name of the fasttext model in the root of the script folder
-fasttext_model_name = "lid.176.bin"
+fasttext_model_name = "lid.176.ftz"
 
 # Folder names to ignore when recursively scanning a path
 ignored_folder_names = []
