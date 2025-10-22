f2147833d99574178b91d5a2d844b6cae21f5697
Zach Stultz <hcazs4@live.com>
2025-10-22 10:16:08 -0500

    Add track types to check in settings

--- settings.py
+++ settings.py
@@ -5,9 +5,14 @@ fasttext_model_name = "lid.176.ftz"
 ignored_folder_names = []
 
 # The list of track languages that will trigger a detection attempt.
-track_languages_to_check = [
+track_types_to_check = [
+    "subtitles",
+]
+
+# The list of track languages that will trigger a detection attempt.
+subtitle_languages_to_check = [
     "zxx",
     "und",
-    # "jpn", # sometimes releasers mark a track as jpn, but they're not actually jpn subtitles
+    # "jpn",  # sometimes releasers mark a track as jpn, but they're not actually jpn subtitles
     # "mul" # sometimes a track is set as multi, but it isn't actually multi
 ]
